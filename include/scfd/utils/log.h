bd1121208fc09f9d743fb0557b8bc20984076184
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2020-10-02 16:36:13 +0300

    log class was moved into format_log_base.h and renamed

--- include/scfd/utils/log.h
+++ include/scfd/utils/log.h
@@ -22,121 +22,12 @@
 #include <stdexcept>
 #include <cstdarg>
 #include <cstdio>
+#include "format_log_base.h"
 
 namespace scfd
 {
 namespace utils
 {
-
-class log
-{
-    char    buf[200];
-public:
-    //INFO_ALL refers to multi-process applications (like MPI) and means message, that must be said distintly by each process (like, 'i'm 1st; i'm second etc')
-    enum t_msg_type { INFO, INFO_ALL, WARNING, ERROR };
-
-    //lesser log level corresponds to more important messages
-    virtual void msg(const std::string &s, t_msg_type mt = INFO, int _log_lev = 1) = 0;
-    void info(const std::string &s, int _log_lev = 1)
-    {
-        msg(s, INFO, _log_lev);
-    }
-    void info_all(const std::string &s, int _log_lev = 1)
-    {
-        msg(s, INFO_ALL, _log_lev);
-    }
-    void warning(const std::string &s, int _log_lev = 1)
-    {
-        msg(s, WARNING, _log_lev);
-    }
-    void error(const std::string &s, int _log_lev = 1)
-    {
-        msg(s, ERROR, _log_lev);
-    }   
-    
-
-    #define LOG__FORMATTED_OUT_V__(METHOD_NAME,LOG_LEV)   \
-        vsprintf(buf, s.c_str(), arguments);              \
-        METHOD_NAME(std::string(buf), LOG_LEV);
-    void v_info_f(int _log_lev, const std::string &s, va_list arguments)
-    {
-        LOG__FORMATTED_OUT_V__(info, _log_lev)
-    }
-    void v_info_all_f(int _log_lev, const std::string &s, va_list arguments)
-    {
-        LOG__FORMATTED_OUT_V__(info_all, _log_lev)
-    }
-    void v_warning_f(int _log_lev, const std::string &s, va_list arguments)
-    {
-        LOG__FORMATTED_OUT_V__(warning, _log_lev)
-    }
-    void v_error_f(int _log_lev, const std::string &s, va_list arguments)
-    {
-        LOG__FORMATTED_OUT_V__(error, _log_lev)
-    }
-    void v_info_f(const std::string &s, va_list arguments)
-    {
-        LOG__FORMATTED_OUT_V__(info, 1)
-    }
-    void v_info_all_f(const std::string &s, va_list arguments)
-    {
-        LOG__FORMATTED_OUT_V__(info_all, 1)
-    }
-    void v_warning_f(const std::string &s, va_list arguments)
-    {
-        LOG__FORMATTED_OUT_V__(warning, 1)
-    }
-    void v_error_f(const std::string &s, va_list arguments)
-    {
-        LOG__FORMATTED_OUT_V__(error, 1)
-    }
-    #undef LOG__FORMATTED_OUT_V__ 
-
-
-    #define LOG__FORMATTED_OUT__(METHOD_NAME,LOG_LEV)   \
-        va_list arguments;                              \
-        va_start ( arguments, s );                      \
-        vsprintf(buf, s.c_str(), arguments);            \
-        METHOD_NAME(std::string(buf), LOG_LEV);         \
-        va_end ( arguments );   
-    void info_f(int _log_lev, const std::string &s, ...)
-    {
-        LOG__FORMATTED_OUT__(info, _log_lev)
-    }
-    void info_all_f(int _log_lev, const std::string &s, ...)
-    {
-        LOG__FORMATTED_OUT__(info_all, _log_lev)
-    }
-    void warning_f(int _log_lev, const std::string &s, ...)
-    {
-        LOG__FORMATTED_OUT__(warning, _log_lev)
-    }
-    void error_f(int _log_lev, const std::string &s, ...)
-    {
-        LOG__FORMATTED_OUT__(error, _log_lev)
-    }
-    void info_f(const std::string &s, ...)
-    {
-        LOG__FORMATTED_OUT__(info, 1)
-    }
-    void info_all_f(const std::string &s, ...)
-    {
-        LOG__FORMATTED_OUT__(info_all, 1)
-    }
-    void warning_f(const std::string &s, ...)
-    {
-        LOG__FORMATTED_OUT__(warning, 1)
-    }
-    void error_f(const std::string &s, ...)
-    {
-        LOG__FORMATTED_OUT__(error, 1)
-    }
-    #undef LOG__FORMATTED_OUT__ 
-
-    //set_verbosity sets maximum level of messages to log
-    //NOTE log_lev doesnot affects errors
-    virtual void set_verbosity(int _log_lev = 1) = 0;
-};
 
 class log_std : public log
 {
